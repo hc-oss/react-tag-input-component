acf10e940fc39e0678016def47072f4d54f5e8e5
Harsh Zalavadiya <harshzalavadiya@users.noreply.github.com>
2022-10-01 13:08:24 +0000

    refactor: :recycle: move to tsup

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,5 @@
-import { css, setup } from "goober";
+import "./styles.css";
+
 import React, { useState } from "react";
 import { useDidUpdateEffect } from "./use-did-update-effect";
 
@@ -24,48 +25,6 @@ export interface TagsInputProps {
     tag?: string;
   };
 }
-
-// initialize goober once
-setup(React.createElement);
-
-const RTIContainer = css({
-  "--rti-bg": "#fff",
-  "--rti-border": "#ccc",
-  "--rti-main": "#3182ce",
-  "--rti-radius": "0.375rem",
-  "--rti-s": "0.5rem",
-  "--rti-tag": "#edf2f7",
-  "--rti-tag-remove": "#e53e3e",
-  "--rti-tag-padding": "0.15rem 0.25rem",
-
-  "*": {
-    boxSizing: "border-box",
-    transition: "all 0.2s ease",
-  },
-
-  alignItems: "center",
-  bg: "var(--rti-bg)",
-  border: "1px solid var(--rti-border)",
-  borderRadius: "var(--rti-radius)",
-  display: "flex",
-  flexWrap: "wrap",
-  gap: "var(--rti-s)",
-  lineHeight: 1.4,
-  padding: "var(--rti-s)",
-
-  "&:focus-within": {
-    borderColor: "var(--rti-main)",
-    boxShadow: "var(--rti-main) 0px 0px 0px 1px",
-  },
-});
-
-const RTIInput = css({
-  border: 0,
-  outline: 0,
-  fontSize: "inherit",
-  lineHeight: "inherit",
-  width: "50%",
-});
 
 const defaultSeprators = ["Enter"];
 
@@ -131,7 +90,7 @@ export const TagsInput = ({
   };
 
   return (
-    <div aria-labelledby={name} className={cc("rti--container", RTIContainer)}>
+    <div aria-labelledby={name} className="rti--container">
       {tags.map(tag => (
         <Tag
           key={tag}
@@ -143,7 +102,7 @@ export const TagsInput = ({
       ))}
 
       <input
-        className={cc("rti--input", RTIInput, classNames?.input)}
+        className={cc("rti--input", classNames?.input)}
         type="text"
         name={name}
         placeholder={placeHolder}
